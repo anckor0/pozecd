619f22f85566f114762c617bdbe8250608c5210e
Arash Zd <65112204+anckor0@users.noreply.github.com>
2023-06-17 12:51:04 +0330

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get('/login', authController.getLogin);
 
-router.get('/signup', authController.getSignup);
+// router.get('/signup', authController.getSignup);
 
 router.post(
   '/login',
@@ -20,49 +20,49 @@ router.post(
   authController.postLogin
 );
 
-router.post(
-  '/signup',
-  [
-    check('email')
-      .isEmail()
-      .withMessage('Please enter a valid email')
-      .custom((value, { req }) => {
-        return User.findOne({ email: value }).then(user => {
-          if (user) {
-            return Promise.reject(
-              'Email already exists, please pick a different one'
-            );
-          }
-        });
-      })
-      .normalizeEmail(),
-    body(
-      'password',
-      'Please enter a password with only numbers and text with at least 5 characters'
-    )
-      .isLength({ min: 5 })
-      .isAlphanumeric()
-      .trim(),
-    body('confirmPassword')
-      .custom((value, { req }) => {
-        if (value !== req.body.password) {
-          throw new Error("Passwords don't match");
-        }
-        return true;
-      })
-      .trim()
-  ],
-  authController.postSignup
-);
+// router.post(
+//   '/signup',
+//   [
+//     check('email')
+//       .isEmail()
+//       .withMessage('Please enter a valid email')
+//       .custom((value, { req }) => {
+//         return User.findOne({ email: value }).then(user => {
+//           if (user) {
+//             return Promise.reject(
+//               'Email already exists, please pick a different one'
+//             );
+//           }
+//         });
+//       })
+//       .normalizeEmail(),
+//     body(
+//       'password',
+//       'Please enter a password with only numbers and text with at least 5 characters'
+//     )
+//       .isLength({ min: 5 })
+//       .isAlphanumeric()
+//       .trim(),
+//     body('confirmPassword')
+//       .custom((value, { req }) => {
+//         if (value !== req.body.password) {
+//           throw new Error("Passwords don't match");
+//         }
+//         return true;
+//       })
+//       .trim()
+//   ],
+//   authController.postSignup
+// );
 
 router.post('/logout', authController.postLogout);
 
-router.get('/reset', authController.getReset);
+// router.get('/reset', authController.getReset);
 
-router.post('/reset', authController.postReset);
+// router.post('/reset', authController.postReset);
 
-router.get('/reset/:token', authController.getNewPassword);
+// router.get('/reset/:token', authController.getNewPassword);
 
-router.post('/new-password', authController.postNewPassword);
+// router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
